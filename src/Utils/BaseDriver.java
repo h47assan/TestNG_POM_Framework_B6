abea8b1afd60f4cfb87cbdeb217147873808af4c
hmk10 <halitmk@gmail.com>
2022-08-12 21:44:29 -0400

    Browser Parameter for Driver

--- src/Utils/BaseDriver.java
+++ src/Utils/BaseDriver.java
@@ -2,8 +2,10 @@ package Utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 
 import java.util.concurrent.TimeUnit;
 
@@ -12,16 +14,22 @@ public class BaseDriver {
     protected WebDriver driver;
 
     @BeforeMethod
-    public void setup() {
+    @Parameters({"browser"})
+    public void setup(String browser) {
 
-        System.setProperty("webdriver.chrome.driver", "/Library/Selenium/chromedriver/chromedriver");
-
-        driver = new ChromeDriver();
-
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.manage().window().maximize();
-
-        driver.get("http://opencart.abstracta.us/");
+        if (browser.equals("chrome")) {
+            System.setProperty("webdriver.chrome.driver", "/Library/Selenium/chromedriver/chromedriver");
+            driver = new ChromeDriver();
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            driver.manage().window().maximize();
+            driver.get("http://opencart.abstracta.us/");
+        } else if (browser.equals("firefox")) {
+            System.setProperty("webdriver.gecko.driver", "/Library/Selenium/geckodriver/geckodriver");
+            driver = new FirefoxDriver();
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            driver.manage().window().maximize();
+            driver.get("http://opencart.abstracta.us/");
+        }
 
     }
 
